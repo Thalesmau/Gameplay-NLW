badac297a5b89866232dad6a92283809a659f763
Thalesmau <thalesmausiq46@gmail.com>
2022-04-18 16:56:27 -0300

    first commit

---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '78%',
+        height: 1,
+        backgroundColor: theme.colors.secondary80,
+        alignSelf: 'flex-end'
+    }
+});
---
+++ src/components/ModalView/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 100
+    },
+    overlay: {
+        flex: 1,
+        backgroundColor: theme.colors.overlay
+    },
+    bar: {
+        width: 39,
+        height: 2,
+        borderRadius: 2,
+        backgroundColor: theme.colors.secondary30,
+        alignSelf: 'center',
+        marginTop: 13
+    },
+    type: {
+        fontFamily: theme.fonts.text400,
+        color: theme.colors.highlight,
+        fontSize: 13,
+        marginBottom: 24,
+    }
+})
