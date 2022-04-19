d9b4e71b936f7250e694192d4a3e1e71532fb78f
Thalesmau <thalesmausiq46@gmail.com>
2022-04-19 10:24:14 -0300

    final session

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+import { useNavigation } from '@react-navigation/native';
+
 
 import {
   Text,
@@ -10,6 +14,7 @@ import {
   KeyboardAvoidingView,
 } from 'react-native';
 
+import { COLLECTION_APPOINTMENTS } from '../../configs/database'
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
@@ -28,6 +33,15 @@ import { Guilds } from '../Guilds';
 export function AppointmentCreate() {
   const [category, setCategory] = useState('');
   const [openGuildsModa, setOpenGuildsModal] = useState(false);
+
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [hour, setHour] = useState('');
+  const [minute, setMinute] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navigation = useNavigation();
+
   const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
 
   function handleOpenGuilds() {
@@ -45,6 +59,26 @@ export function AppointmentCreate() {
 
   function handleCategorySelect(categoryId: string) {
     setCategory(categoryId);
+  }
+
+  async function handleSave() {
+    const newAppointment = {
+      id: uuid.v4(),
+      guild,
+      category,
+      date: `${day}/${month} às ${hour}:${minute}h`,
+      description
+    };
+
+    const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const appointments = storage ? JSON.parse(storage) : [];
+
+    await AsyncStorage.setItem(
+      COLLECTION_APPOINTMENTS,
+      JSON.stringify([...appointments, newAppointment])
+    );
+
+    navigation.navigate('Home');
   }
 
   return (
@@ -76,7 +110,7 @@ export function AppointmentCreate() {
               <View style={styles.select}>
                 {
                   guild.icon
-                    ? <GuildIcon />
+                    ? <GuildIcon guildId={guild.id} iconId={guild.icon} />
                     : <View style={styles.image} />
                 }
 
@@ -105,11 +139,17 @@ export function AppointmentCreate() {
                 </Text>
 
                 <View style={styles.column}>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setDay}
+                  />
                   <Text style={styles.divider}>
                     /
                   </Text>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setMonth}
+                  />
                 </View>
               </View>
 
@@ -119,11 +159,17 @@ export function AppointmentCreate() {
                 </Text>
 
                 <View style={styles.column}>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setHour}
+                  />
                   <Text style={styles.divider}>
                     :
                   </Text>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setMinute}
+                  />
                 </View>
               </View>
             </View>
@@ -143,10 +189,14 @@ export function AppointmentCreate() {
               maxLength={100}
               numberOfLines={5}
               autoCorrect={false}
+              onChangeText={setDescription}
             />
 
             <View style={styles.footer}>
-              <Button title="Agendar" />
+              <Button 
+              title="Agendar" 
+              onPress={handleSave}
+              />
             </View>
           </View>
         </ScrollView>
